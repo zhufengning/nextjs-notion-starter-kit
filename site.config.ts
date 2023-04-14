2411f6bb8fcdff2a50303213c6bf6bbea59e67c2
zhufengning <renhaihao@outlook.com>
2023-04-15 00:43:58 +0800

    add default cover

--- site.config.ts
+++ site.config.ts
@@ -26,7 +26,7 @@ export default siteConfig({
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
   defaultPageIcon: null,
-  defaultPageCover: null,
+  defaultPageCover: "/101733274_p0.png",
   defaultPageCoverPosition: 0.5,
 
   // whether or not to enable support for LQIP preview images (optional)
